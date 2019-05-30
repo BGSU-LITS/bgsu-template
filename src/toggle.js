ca01dd510a03ccac94c7ed96ada7dc3d3a7c64b4
John Kloor <john@kloor.org>
2019-05-30 16:32:47 -0400

    Fix missing toggle classes, and add support for user information to headers.

--- src/toggle.js
+++ src/toggle.js
@@ -12,8 +12,6 @@ export function setup(
     Array.prototype.forEach.call(
         document.querySelectorAll(selector),
         function (element) {
-            element.classList.add('toggle-control');
-
             new Toggle(
                 document.getElementById(element.dataset.toggle),
                 config
