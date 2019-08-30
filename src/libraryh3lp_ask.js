b79af6099a0b67b74db03dc97bc9669182e43d6d
John Kloor <john@kloor.org>
2019-08-30 10:49:11 -0400

    Add function to display tag clouds for LibraryH3lp Ask.

--- src/libraryh3lp_ask.js
+++ src/libraryh3lp_ask.js
@@ -3,8 +3,50 @@ import coreInput from '@nrk/core-input';
 
 import css from './css/libraryh3lp_ask.css';
 
+export function cloud(selector, name) {
+    var url = 'https://' + name + '.ask.libraryh3lp.com';
+
+    jsonp(
+        url + '/topics',
+        function(err, data) {
+            if (err) {
+                return;
+            }
+
+            var max, min;
+
+            for (var count = 0; count < data.length; count++) {
+                if (!max || data[count].count > max) {
+                    max = data[count].count;
+                }
+
+                if (!min || data[count].count < min) {
+                    min = data[count].count;
+                }
+            }
+
+            var items = data.map(function(item) {
+                var size = (item.count - min) / (max - min);
+
+                return '<a href="' +
+                    url + item.url + '" style="font-size:' +
+                    Math.round(24 * size + 12 * (1 - size)) + 'pt">' +
+                    item.name + '</a> ';
+            });
+
+            Array.prototype.forEach.call(
+                document.querySelectorAll(selector),
+                function(element) {
+                    element.classList.add(css.libraryh3lp_ask_cloud);
+                    element.innerHTML = items.join('');
+                }
+            );
+        }
+    );
+}
+
 export function setup(selector, name) {
-    var url = 'https://' + name + '.ask.libraryh3lp.com/';
+    var url = 'https://' + name + '.ask.libraryh3lp.com';
 
     Array.prototype.forEach.call(
         document.querySelectorAll(selector),
@@ -36,7 +78,7 @@ export function setup(selector, name) {
                 }
 
                 debounce = jsonp(
-                    url + 'instant?q=' + value,
+                    url + '/instant?q=' + value,
                     function(err, data) {
                         if (err) {
                             coreInput(element, '');
