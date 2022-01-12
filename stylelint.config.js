cd2231aa34adfc2a0d5631f09e4195248c60ef82
John Kloor <john@kloor.org>
2022-01-12 14:54:38 -0500

    Remove stylelint options duplicated by standard, and alphabetize.

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,32 +1,17 @@
 module.exports = {
     extends: 'stylelint-config-standard',
     rules: {
-        indentation: 4,
-        'max-line-length': 80,
-
-        'font-family-name-quotes': 'always-where-recommended',
-        'function-url-quotes': 'always',
-        'selector-attribute-quotes': 'always',
-        'string-quotes': 'double',
-
-        'at-rule-no-vendor-prefix': true,
-        'media-feature-name-no-vendor-prefix': true,
-        'property-no-vendor-prefix': true,
-        'selector-no-vendor-prefix': true,
-        'value-no-vendor-prefix': true,
-
-        'declaration-no-important': true,
-        'selector-no-qualifying-type': true,
-
-        'font-weight-notation': 'numeric',
-
-        'selector-pseudo-class-no-unknown': [true, {
-            ignorePseudoClasses: ['global'],
-        }],
-
         'at-rule-empty-line-before': ['always', {
             except: ['first-nested'],
             ignore: ['after-comment'],
+        }],
+        'declaration-no-important': true,
+        indentation: 4,
+        'font-weight-notation': 'numeric',
+        'max-line-length': 80,
+        'selector-no-qualifying-type': true,
+        'selector-pseudo-class-no-unknown': [true, {
+            ignorePseudoClasses: ['global'],
         }]
     }
 };
