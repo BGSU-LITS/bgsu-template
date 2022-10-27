ab4c79ee4731428cdcc947215a8073577f7eb60f
John Kloor <john@kloor.org>
2022-10-27 14:39:59 -0400

    Update tippy.js for v2.

--- src/tippy.js
+++ src/tippy.js
@@ -1,22 +1,31 @@
+/**
+ * Provides improved tooltips for elements.
+ * @modules tippy
+ */
+
 import tippy from 'tippy.js';
 import 'tippy.js/dist/tippy.css';
 import 'tippy.js/themes/light-border.css';
-import './css/tippy.css';
+import css from './scss/tippy.scss';
 
-export function setup(selector = null, config = {}) {
-    selector = selector || '[title]';
+/**
+ * Sets up a tooltip for selected elements' titles.
+ * @param {string} selector Selector used to identify elements. The elements
+ *    should have a title attribute with the content for the tooltip.
+ * @param {Object} [config] Additional configuration values, see:
+ *    https://atomiks.github.io/tippyjs/v6/all-props/
+ */
+export function setup(selector = '[title]', config = {}) {
+    document.querySelectorAll(selector).forEach((element) => {
+        const instance = tippy(element, {
+            arrow: true,
+            content: element.getAttribute('title'),
+            placement: 'right',
+            theme: 'light-border',
+            ...config,
+        });
 
-    config = Object.assign(
-        { arrow: true, placement: 'right', theme: 'light-border' },
-        config
-    );
-
-    Array.prototype.forEach.call(
-        document.querySelectorAll(selector),
-        function(element) {
-            config.content = element.getAttribute('title');
-            element.setAttribute('title', '');
-            tippy(element, config);
-        }
-    );
+        instance.popper.classList.add(css.tippy);
+        element.setAttribute('title', '');
+    });
 }
--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = function config(environment, argv) {
         'libchat',
         // 'tablist',
         'template',
-        // 'tippy',
+        'tippy',
         'toggle',
     ];
 
